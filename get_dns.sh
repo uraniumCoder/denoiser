edbcc21e0be1dd36fade15761e38dc42be681e07
uraniumCoder <williamzhaohaitong7@gmail.com>
2022-12-06 21:59:34 -0500

    fix script

--- get_dns.sh
+++ get_dns.sh
@@ -1,4 +1,4 @@
-!cp /content/drive/MyDrive/tinyml\ final\ project/zipfiles/dns/generated.zip /content/denoiser/dataset/
-!cd /content/denoiser/dataset; unzip generated.zip
-!mkdir /content/denoiser/dataset/dns
-!mv /content/denoiser/dataset/generated/* /content/denoiser/dataset/dns
+cp /content/drive/MyDrive/tinyml\ final\ project/zipfiles/dns/generated.zip /content/denoiser/dataset/
+cd /content/denoiser/dataset; unzip generated.zip
+mkdir /content/denoiser/dataset/dns
+mv /content/denoiser/dataset/generated/* /content/denoiser/dataset/dns
